4834c111533890fad27ac91956d18ee4bae5e763
mcgillshakore <mcgills2093@gmail.com>
2020-11-20 13:45:10 -0600

    final demo

--- public/app.js
+++ public/app.js
@@ -19,12 +19,52 @@ auth.onAuthStateChanged(user => {
     if  (user) {
         // signed in
         whenSignedIn.hidden = false;
+        firestore.hidden = false;
         whenSignedOut.hidden = true;
         userDetails.innerHTML = `<h3> Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
     }else {
         // not signed in
         whenSignedIn.hidden = true;
+        firestore.hidden = true;
         whenSignedOut.hidden = false;
         userDetails.innerHTML = '';
     }
 });
+
+const db = firebase.firestore(); //Firestore SDK
+
+const createThing = document.getElementById('createThing');
+const thingsList = document.getElementById('thingsList');
+
+let thingsRef;
+let unsubscribe;
+
+auth.onAuthStateChanged( user => {
+    if (user) {
+        thingsRef = db.collection ('things')
+
+        createThing.onclick = () => {
+
+            const { serverTimestamp } = firebase.firestore.FieldValue;
+
+            thingsRef.add({
+                uid: user.uid,
+                name: faker.commerce.productName(),
+                createdAt: serverTimestamp()
+            });
+        }
+
+        unsubscribe = thingsRef
+            .where('uid', '==', user.uid)
+            .orderBy('createdAt')
+            .onSnapshot(querySnapshot => {
+                const items = querySnapshot.docs.map(doc => {
+                    return `<li> ${ doc.data().name }</li>`
+                });
+
+                thingsList.innerHTML = items.join('');
+            });
+    } else {
+        unsubscribe && unsubscribe();
+    }
+})
